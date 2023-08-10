486fcccedbea88c989509f62f80eb9bbb5a676e4
yi_t <marvelousme@163.com>
2023-08-10 15:15:17 +0800

    style: order

--- app/links.ts
+++ app/links.ts
@@ -56,6 +56,16 @@ const _default: CategoryData = {
   code: 200,
   message: "success",
   data: [
+    new category("Go", "/go.svg", [
+      new siteLink("go doc", "https://golang.google.cn/", "/go.svg", "Go doc,Effective Go,官方文档,下载链接,example"),
+      new siteLink("go-zero", "https://go-zero.dev/", "/go-zero.svg", "a web and rpc framework,go微服务框架"),
+      new siteLink("beego-v2 doc", "https://beego.programnotes.cn", "/beego.jpg", "beego,go web框架,文档")]),
+
+    new category("AI", "/ai-resize-20.png", [
+      new siteLink("魔搭", "https://modelscope.cn/", "/modelscopeIcon.png", "开源的模型即服务共享平台,为泛AI开发者提供灵活、易用、低成本的一站式模型服务产品,让模型应用更简单"),
+      new siteLink("通义千问", "https://qianwen.aliyun.com/chat", "/qianwen.png", "阿里云研发的通义千问大模型"),
+    ],),
+
     new category("Tool", "/toml.svg", [
       new siteLink("frp", "https://github.com/fatedier/frp", "/accenture.svg", "A fast reverse proxy,反向代理工具"),
       new siteLink("flomo", "https://flomoapp.com", "/flomologo.png", "notes tool,全平台的卡片笔记，聚焦帮你记录更多想法与灵感，以及更好回顾过往记录"),
@@ -63,30 +73,28 @@ const _default: CategoryData = {
       new siteLink("simple icon", "https://github.com/simple-icons/simple-icons", "/thirteen.svg", "svg icon"),
       new siteLink("gitpod", "https://gitpod.io/workspaces", "/Gitpod.svg", "browser editor:vscode,idea,goland...浏览器里用vscode,idea等"),
       new siteLink("m3u8-downloader", "https://www.yeyulingfeng.com/tools/m3u8-downloader/", "/docusign.svg", "m3u8 video downloader,视频下载器")]),
+
+    new category("Remote", "/remote.svg", [
+      new siteLink("eleduck", "https://eleduck.com/", "/eleduck.png", "eleduck,电鸭,远程工作社区"),
+    ],),
+
     new category("Serverless", "/serverless.svg", [
       new siteLink("Netlify", "https://functions.netlify.com/", "/netlify.svg", "Serverless platforms,网站托管"),
       new siteLink("Vercel", "https://vercel.com/", "/vercel.svg", "Serverless platforms,网站托管"),
       new siteLink("aliyun FC", "https://help.aliyun.com/zh/fc/", "/aliyun.svg", "阿里云函数计算(FC),事件驱动的全托管计算服务。通过函数计算，无需管理服务器等基础设施，只需编写代码并上传")]),
-    new category("Go", "/go.svg", [
-      new siteLink("go doc", "https://golang.google.cn/", "/go.svg", "Go doc,Effective Go,官方文档,下载链接,example"),
-      new siteLink("go-zero", "https://go-zero.dev/", "/go-zero.svg", "a web and rpc framework,go微服务框架"),
-      new siteLink("beego-v2 doc", "https://beego.programnotes.cn", "/beego.jpg", "beego,go web框架,文档")]),
+
     new category("Frontend", "/frontify.svg", [
       new siteLink("Next.js", "https://nextjs.org/", "/next.svg", "React framework,React 框架"),
       new siteLink("xxxx", "https://github.com/serverless-coding/frontend-nav", "/acer.svg", ""),
       new siteLink("xxxx", "https://github.com/serverless-coding/frontend-nav", "/snyk.svg", "")]),
+
     new category("Article", "/bloglovin.svg", [
       new siteLink("notes", "https://programnotes.cn", "https://programnotes.cn/Image/logo.png", "programnotes")]),
+
     new category("CodeStyle", "/CodePen.svg", [
       new siteLink("uber/go", "https://github.com/xxjwxc/uber_go_guide_cn", "/go.svg", "uber/go code style guide,Uber go规范"),
-      new siteLink("shimohq/react", "https://github.com/shimohq/react-cookbook", "/React.svg", "shimo react cookbook,石墨文档 react")],),
-    new category("Remote", "/remote.svg", [
-      new siteLink("eleduck", "https://eleduck.com/", "/eleduck.png", "eleduck,电鸭,远程工作社区"),
-    ],),
-    new category("AI", "/ai-resize-20.png", [
-      new siteLink("魔搭", "https://modelscope.cn/", "/modelscopeIcon.png", "开源的模型即服务共享平台,为泛AI开发者提供灵活、易用、低成本的一站式模型服务产品,让模型应用更简单"),
-      new siteLink("通义千问", "https://qianwen.aliyun.com/chat", "/qianwen.png", "阿里云研发的通义千问大模型"),
-    ],)
+      new siteLink("shimohq/react", "https://github.com/shimohq/react-cookbook", "/React.svg", "shimo react cookbook,石墨文档 react")],)
+
   ]
 }
 
