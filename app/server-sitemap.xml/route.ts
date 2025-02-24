1e2030a763ca46d2a79e97e82de233e59bf1d240
yi_t <marvelousme@163.com>
2025-02-25 01:13:13 +0800

    doc

--- app/server-sitemap.xml/route.ts
+++ app/server-sitemap.xml/route.ts
@@ -1,7 +1,7 @@
 import { getServerSideSitemap } from 'next-sitemap'
 
 export async function GET() {
-  const posts = ['','page/about', 'page/aigames']
+  const posts = ['', 'page/about', 'page/aigames', "/page/mirror"]
 
   const fields = posts.map((slug) => ({
     loc: `https://nav.programnotes.cn/${slug}`,
