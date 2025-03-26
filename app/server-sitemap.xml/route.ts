5ec53d6f63e8250f50d7bf703c9008e87c41621a
yi_t <marvelousme@163.com>
2025-03-26 18:40:10 +0800

    fix: sitemap

--- app/server-sitemap.xml/route.ts
+++ app/server-sitemap.xml/route.ts
@@ -1,12 +1,24 @@
 import { getServerSideSitemap } from 'next-sitemap'
+import linksData from '../../data/links.json'
 
 export async function GET() {
-  const posts = ['', 'page/about', 'page/aigames', "/page/mirror"]
+  const staticPosts = ['', 'page/about', 'page/aigames', "page/mirror"];
 
-  const fields = posts.map((slug) => ({
+  const dynamicPosts = linksData.data.reduce((acc: string[], category) => {
+    category.links.forEach(link => {
+      if (link.page) {
+        acc.push(`page/${link.page.replace('.md', '')}`);
+      }
+    });
+    return acc;
+  }, []);
+
+  const posts = [...staticPosts, ...dynamicPosts];
+
+  const fields = posts.map((slug: string) => ({
     loc: `https://nav.programnotes.cn/${slug}`,
     lastmod: new Date().toISOString(),
-  }))
+  }));
 
-  return getServerSideSitemap(fields)
+  return getServerSideSitemap(fields);
 }
--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   siteUrl: 'https://nav.programnotes.cn',
   generateRobotsTxt: true,
-  exclude: ['/server-sitemap.xml'], // <= exclude here
   robotsTxtOptions: {
     additionalSitemaps: [
       'https://nav.programnotes.cn/server-sitemap.xml', // <= sitemap route here
